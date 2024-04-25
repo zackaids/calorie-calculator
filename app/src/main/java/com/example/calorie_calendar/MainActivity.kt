a33014a5f97f1ae10232f2820b2b213e547ee5b9
zackaids <84846343+zackaids@users.noreply.github.com>
2024-04-25 13:47:26 -0400

    calendar added

--- app/src/main/java/com/example/calorie_calendar/MainActivity.kt
+++ app/src/main/java/com/example/calorie_calendar/MainActivity.kt
@@ -36,9 +36,5 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
-
-
-
     }
-
 }
--- app/src/main/java/com/example/calorie_calendar/ui/calendar/CalendarFragment.kt
+++ app/src/main/java/com/example/calorie_calendar/ui/calendar/CalendarFragment.kt
@@ -4,9 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CalendarView
+import android.widget.CalendarView.OnDateChangeListener
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.calorie_calendar.R
 import com.example.calorie_calendar.databinding.FragmentCalendarBinding
 
 class CalendarFragment : Fragment() {
@@ -14,21 +18,26 @@ class CalendarFragment : Fragment() {
 
     private val binding get() = _binding!!
 
+    lateinit var calendarView: CalendarView
+    lateinit var dateTV: TextView
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(CalendarViewModel::class.java)
-
         _binding = FragmentCalendarBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textCalendar
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        dateTV = root.findViewById(R.id.text_calendar)
+        calendarView = root.findViewById(R.id.calendarView)
+
+        calendarView.setOnDateChangeListener(OnDateChangeListener { view, year, month, dayOfMonth ->
+            val Date = (dayOfMonth.toString() + "-" +
+                    (month + 1) + "-" + year)
+
+            dateTV.setText(Date)
+        })
+
         return root
 
     }
