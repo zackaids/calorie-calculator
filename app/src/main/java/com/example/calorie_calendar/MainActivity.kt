97cfd152e346cc529d671c0590d8344189450d00
LavdrimIslami <lavdrim.islami280@gmail.com>
2024-04-26 13:38:03 -0400

    added some buttons

--- app/src/main/java/com/example/calorie_calendar/MainActivity.kt
+++ app/src/main/java/com/example/calorie_calendar/MainActivity.kt
@@ -39,4 +39,5 @@ class MainActivity : AppCompatActivity() {
 
 
     }
+
 }
--- app/src/main/java/com/example/calorie_calendar/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/calorie_calendar/ui/home/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.example.calorie_calendar.ui.home
 
+import android.app.Activity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
