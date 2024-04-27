c1821662a61d9f14817a6d016f8b3e850bcfbc9c
zackaids <84846343+zackaids@users.noreply.github.com>
2024-04-26 23:15:16 -0400

    main screen buttons not work

--- app/src/main/java/com/example/calorie_calendar/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/calorie_calendar/ui/home/HomeFragment.kt
@@ -1,14 +1,18 @@
 package com.example.calorie_calendar.ui.home
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.calorie_calendar.R
 import com.example.calorie_calendar.databinding.FragmentHomeBinding
+import com.example.calorie_calendar.ui.add.AddFragment
 
 class HomeFragment : Fragment() {
 
@@ -23,16 +27,20 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+//        val homeViewModel =
+//            ViewModelProvider(this).get(HomeViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        val btnAdd = root.findViewById<Button>(R.id.imageButton)
+        btnAdd.setOnClickListener {
+            AddFragment()
         }
+//        val textView: TextView = binding.textHome
+//        homeViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
         return root
     }
 
